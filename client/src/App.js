54a8d0efdf7a37c050a69f720d4a025ac93d8805
Ben0qq <damidbb@gmailcom>
2021-04-09 21:03:14 +0200

    deleted the example

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
-  const [responseFromApi, setResponse] = useState({})
+  const [responseFromApi] = useState({})
 
   const fetchData = async() => {
     fetch('/api/hello')
@@ -22,53 +20,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </span>
         <a style={{ fontSize: 24 }}>
         Msg from API: <span style={{ color: 'red' }}>{responseFromApi.header}</span>
         </a>
       </header>
-
     </div>
   );
 }
