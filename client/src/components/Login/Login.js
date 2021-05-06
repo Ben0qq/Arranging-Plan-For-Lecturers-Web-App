ab662bc81b80687f2e65ac555c2bd2661d090230
Ben0qq <damidbb@gmailcom>
2021-05-07 00:14:08 +0200

    login done

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -2,15 +2,35 @@ import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import './Login.css';
 import { useDispatch } from 'react-redux';
-import { setAppState } from './loginSlice';
+import { requestLogin } from './loginSlice';
+import React, {useState} from 'react';
 
 export function Login() {
     const dispatch = useDispatch();
+    const [loginData, setLoginData] = useState({
+        login: '',
+        password: ''
+    })
+
+    const handleChange = (prop) => (event) => {
+        setLoginData({ ...loginData, [prop]: event.target.value})
+    }
+
     return (
         <div className="loginWorkArea">
-            <Input placeholder="Login" inputProps={{ 'aria-label': 'login field' }} />
-            <Input style={{margin: "10px"}} placeholder="Password" inputProps={{ 'aria-label': 'password field' }} />
-            <Button variant="contained" color="primary" onClick={() => dispatch(setAppState("plan"))}>
+            <Input 
+                placeholder="Login" 
+                inputProps={{ 'aria-label': 'login field' }}
+                onChange={handleChange('login')}
+                 />
+            <Input 
+                type='password' 
+                style={{margin: "10px"}} 
+                placeholder="Password" 
+                inputProps={{ 'aria-label': 'password field' }}
+                onChange={handleChange('password')}
+                 />
+            <Button variant="contained" color="primary" onClick={() => dispatch(requestLogin(loginData.login, loginData.password))}>
                 Tu kiedyś będzie logowanie
             </Button>
         </div>
--- client/src/components/Login/loginSlice.js
+++ client/src/components/Login/loginSlice.js
@@ -1,16 +1,42 @@
-import {  createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  appState: 'login'
+  appState: 'login',
+  status: 'idle'
 };
+
+export const requestLogin = createAsyncThunk(
+  'login/requestLogin',
+  async(login, password) => {
+    const response = await axios.post(
+    'http://localhost:5000/api/users/login',
+    {
+      "email": login,
+      'password': password
+    }
+  )
+  setAppState('calendar');
+    return response.data;
+  }
+)
 
 export const loginSlice = createSlice({
   name: 'login',
   initialState,
   reducers: {
-	setAppState: (state, action) => {
-	  state.appState = action.payload;
-	},
+    setAppState: (state, action) => {
+      state.appState = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(requestLogin.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(requestLogin.fulfilled, (state) => {
+        state.status = 'idle';
+      });
   },
 });
 
