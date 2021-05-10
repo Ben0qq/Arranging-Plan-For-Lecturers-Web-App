9090757b2226c477b85b3f7ea59a5debcf577842
Arek <52425132+carzynsky@users.noreply.github.com>
2021-05-10 22:12:25 +0200

    [master] Add docker files.

--- api/server.js
+++ api/server.js
@@ -29,19 +29,3 @@ process.on(`unhandledRejection`, err => {
   console.log(err.name, err.message)
   server.close(() => process.exit(1))
 })
-
-
-// app.use(bodyParser.json());
-
-// app.get('/api/hello', (req, res) => {
-//   res.send({ header: 'Testowa wiadomość' });
-// });
-
-// app.post('/api/data', (req, res) => {
-//   console.log(req.body);
-//   res.send(
-//     `I received your POST request. This is what you sent me: ${req.body.post}`,
-//   );
-// });
-
-// app.listen(port, () => console.log(`Listening on port ${port}`));
